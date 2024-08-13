7ca4d968505b70a266bb91ca4649b322f97ff6af
Enoc Dominguez <enocescazan@hotmail.com>
2024-08-13 15:21:30 -0600

    Add price animation to popular price

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import CheckIcon from "@/assets/check.svg";
+import { motion } from "framer-motion";
 import { twMerge } from "tailwind-merge";
 
 const pricingTiers = [
@@ -92,9 +95,20 @@ export const Pricing = () => {
                   </h3>
                   {popular && (
                     <div className="inline-flex text-sm px-4 py-1.5 rounded-xl border border-white/20">
-                      <span className="bg-[linear-gradient(to_right,#DD7DDF,#E1CD16,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF)] text-transparent bg-clip-text font-medium">
+                      <motion.span
+                        className="bg-[linear-gradient(to_right,#DD7DDF,#E1CD16,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF,#E1CD16,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF)] [background-size:200%] text-transparent bg-clip-text  font-medium"
+                        animate={{
+                          backgroundPositionX: "100%",
+                        }}
+                        transition={{
+                          repeat: Infinity,
+                          ease: "linear",
+                          repeatType: "loop",
+                          duration: 1,
+                        }}
+                      >
                         Popular
-                      </span>
+                      </motion.span>
                     </div>
                   )}
                 </div>
