7bb59f35d60341ffc0741f3eab3e879b93ebdf42
Enoc Dominguez <enocescazan@hotmail.com>
2024-08-13 14:05:43 -0600

    Update image attributes to individual lines

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,10 +1,10 @@
-import acmeLogo from "@/assets/logo-acme.png"
-import quantumLogo from "@/assets/logo-quantum.png"
-import echoLogo from "@/assets/logo-echo.png"
-import celestialLogo from "@/assets/logo-celestial.png"
-import pulseLogo from "@/assets/logo-pulse.png"
-import apexLogo from "@/assets/logo-apex.png"
-import Image from "next/image"
+import acmeLogo from "@/assets/logo-acme.png";
+import apexLogo from "@/assets/logo-apex.png";
+import celestialLogo from "@/assets/logo-celestial.png";
+import echoLogo from "@/assets/logo-echo.png";
+import pulseLogo from "@/assets/logo-pulse.png";
+import quantumLogo from "@/assets/logo-quantum.png";
+import Image from "next/image";
 
 export const LogoTicker = () => {
   return (
@@ -12,15 +12,39 @@ export const LogoTicker = () => {
       <div className="container">
         <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
           <div className="flex gap-14 flex-none">
-            <Image src={acmeLogo} alt="Acme Logo" className="logo-ticker-image" />
-            <Image src={quantumLogo} alt="QuantumLogo Logo" className="logo-ticker-image" />
-            <Image src={echoLogo} alt="EchoLogo Logo" className="logo-ticker-image" />
-            <Image src={celestialLogo} alt="CelestialLogo Logo" className="logo-ticker-image" />
-            <Image src={pulseLogo} alt="PulseLogo Logo" className="logo-ticker-image" />
-            <Image src={apexLogo} alt="ApexLogo Logo" className="logo-ticker-image" />
+            <Image
+              src={acmeLogo}
+              alt="Acme Logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={quantumLogo}
+              alt="QuantumLogo Logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={echoLogo}
+              alt="EchoLogo Logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={celestialLogo}
+              alt="CelestialLogo Logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={pulseLogo}
+              alt="PulseLogo Logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={apexLogo}
+              alt="ApexLogo Logo"
+              className="logo-ticker-image"
+            />
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 };
